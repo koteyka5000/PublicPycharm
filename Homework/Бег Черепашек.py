c8f3668c4d968ed1b1b88bb43f2333989f4024b1
koteyka5000 <romacat5000@gmail.com>
2021-07-31 16:33:29 +0300

    Alpha 0.3 (Возможны баги)

--- Homework/Бег Черепашек.py
+++ Homework/Бег Черепашек.py
@@ -1,56 +1,74 @@
 import turtle
 import random
-size = int(input('Размер поля: '))
+
+
+def get_input(): return int(input('Размер поля: '))
+
+
+bgcolor = 'green'
+
 sc = turtle.getscreen()
-linet = turtle.Turtle()
+
+linet = turtle.Turtle()  # Создаём черепашек
 t1 = turtle.Turtle()
 t2 = turtle.Turtle()
 t3 = turtle.Turtle()
 t4 = turtle.Turtle()
 turtles = (t1, t2, t3, t4)
-for q in turtles:
-    q.penup()
-sc.bgcolor('green')
 
-t2.goto(0, 40)
-t3.goto(0, 80)
-t4.goto(0, 120)
+sc.bgcolor(bgcolor)  # Цвет фона
 
-t1.color('yellow')
-t2.color('red')
-t3.color('blue')
 
-linet.goto(size, 0)
-linet.left(90)
-linet.forward(129)
+def set_turtles():  # Ставим черепашек на место и задаём им цвет
+    for q in turtles:  q.penup()
+    t2.goto(0, 40)
+    t3.goto(0, 80)
+    t4.goto(0, 120)
 
-for q in turtles:
-    q.pendown()
+    t1.color('yellow')
+    t2.color('red')
+    t3.color('blue')
+    for q in turtles:  q.pendown()
 
-t1x = 0
-t2x = 0
-t3x = 0
-t4x = 0
 
-while t1x < size or t2x < size or t3x < size or t4x < size:
-    for q in turtles:
-        q.forward(random.uniform(0, 10))
-        t1x = t1.xcor()
-        t2x = t2.xcor()
-        t3x = t3.xcor()
-        t4x = t4.xcor()
-        print(t1x)
-t1x = t1.xcor()
-t2x = t2.xcor()
-t3x = t3.xcor()
-t4x = t4.xcor()
-mtx = max(t1x, t2x, t3x, t4x)
-if t1x == mtx:
-    print('YELLOW IS WINNER')
-elif t2x == mtx:
-    print('RED IS WINNER')
-elif t3x == mtx:
-    print('BLUE IS WINNER')
-elif t4x == mtx:
-    print('BLACK IS WINNER')
+def draw_line(size):  # Чертим разметку
+    linet.up()
+    linet.goto(size, 0)
+    linet.down()
+    linet.left(90)
+    while linet.ycor() != 120:
+        linet.color('black')
+        linet.forward(5)
+        linet.color('white')
+        linet.forward(5)
+    linet.hideturtle()
+
+
+def main(size):  # Главный процесс
+    set_turtles()
+    draw_line(size)
+    t1x, t2x, t3x, t4x = 0, 0, 0, 0
+    while t1x < size or t2x < size or t3x < size or t4x < size:
+        for q in turtles:
+            q.forward(random.uniform(0, 5))
+            t1x = t1.xcor()
+            t2x = t2.xcor()
+            t3x = t3.xcor()
+            t4x = t4.xcor()
+    get_winner(t1x, t2x, t3x, t4x)
+
+
+def get_winner(t1x, t2x, t3x, t4x):  # Отображаем победителя
+    mtx = max(t1x, t2x, t3x, t4x)
+    if t1x == mtx:
+        print('YELLOW IS WINNER')
+    elif t2x == mtx:
+        print('RED IS WINNER')
+    elif t3x == mtx:
+        print('BLUE IS WINNER')
+    elif t4x == mtx:
+        print('BLACK IS WINNER')
+
+
+main(get_input())
 sc.mainloop()
